8ae4a6593d28e5b2df8a4106d8bb170de202eb81
Ferenc Janky <fecjanky@gmail.com>
2017-03-11 13:08:25 +0100

    deleted stubs

--- app/src/main/java/com/inspirationlogical/receipt/service/PrintServices.java
+++
@@ -1,7 +0,0 @@
-package com.inspirationlogical.receipt.service;
-
-/**
- * Created by Ferenc on 2017. 03. 11..
- */
-public interface PrintServices {
-}
--- app/src/main/java/com/inspirationlogical/receipt/utility/ReceiptXMLtoPDF.java
+++
@@ -1,7 +0,0 @@
-package com.inspirationlogical.receipt.utility;
-
-/**
- * Created by Ferenc on 2017. 03. 11..
- */
-public class ReceiptXMLtoPDF {
-}
