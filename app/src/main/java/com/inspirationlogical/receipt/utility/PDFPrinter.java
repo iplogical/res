f544c75609b59a7c99471ddd0d39098ae08ea31e
Ferenc Janky <fecjanky@gmail.com>
2017-03-11 23:21:44 +0100

    print code added

--- app/src/main/java/com/inspirationlogical/receipt/utility/PDFPrinter.java
+++ app/src/main/java/com/inspirationlogical/receipt/utility/PDFPrinter.java
@@ -1,7 +1,38 @@
 package com.inspirationlogical.receipt.utility;
 
+import javax.print.*;
+import javax.print.attribute.AttributeSet;
+import javax.print.attribute.HashPrintRequestAttributeSet;
+import javax.print.attribute.PrintRequestAttributeSet;
+import java.awt.*;
+import java.io.InputStream;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 /**
  * Created by Ferenc on 2017. 03. 11..
  */
 public class PDFPrinter {
+    private  PrintService service;
+    PDFPrinter(){
+        List<PrintService> service = Arrays.asList(PrintServiceLookup.lookupPrintServices(null,null));
+        List<PrintService> filtered = service.stream()
+                .filter((s) -> s.getName().contains("Epson-TM-T20II")).collect(Collectors.toList());
+        if(filtered.isEmpty()) {
+            throw new RuntimeException("Couldn't find Epson-TM-T20II printer");
+        }
+        this.service = filtered.get(0);
+    }
+
+    void print(InputStream pdf) {
+        try {
+            DocPrintJob job = service.createPrintJob();
+            Doc doc = new SimpleDoc(pdf, DocFlavor.INPUT_STREAM.PDF, null);
+            PrintRequestAttributeSet aset = new HashPrintRequestAttributeSet();
+            job.print(doc, aset);
+        }catch (PrintException e){
+            throw new RuntimeException(e);
+        }
+    }
 }
---
+++ app/src/test/java/com/inspirationlogical/receipt/utility/PDFPrinterTest.java
@@ -0,0 +1,51 @@
+package com.inspirationlogical.receipt.utility;
+
+import com.inspirationlogical.receipt.model.BuildTestSchemaRule;
+import com.inspirationlogical.receipt.model.EntityManagerFactoryRule;
+import com.inspirationlogical.receipt.model.adapter.ReceiptAdapter;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+
+import javax.persistence.EntityManager;
+import java.io.*;
+import java.util.Arrays;
+
+/**
+ * Created by Ferenc on 2017. 03. 11..
+ */
+public class PDFPrinterTest {
+    private EntityManager manager;
+
+    @Rule
+    public final EntityManagerFactoryRule factory = new EntityManagerFactoryRule();
+
+    @Rule
+    public final BuildTestSchemaRule schema = new BuildTestSchemaRule();
+
+    @Test
+    public void test_dummy(){
+        try {
+            ReceiptAdapter ra = new ReceiptAdapter(schema.getReceiptSaleOne(), manager);
+            ra.close(Arrays.asList());
+            ByteArrayOutputStream out = new ByteArrayOutputStream();
+            ReceiptXMLToPDF.convertToPDF(out,
+                    new FileInputStream("src/main/resources/schema/receipt_epsonTMT20II.xsl"),
+                    ReceiptToXML.ConvertToStream(ra)
+            );
+            InputStream pdf_in = new ByteArrayInputStream(out.toByteArray(),0,out.size());
+            new PDFPrinter().print(pdf_in);
+        } catch(Exception e){
+            throw new RuntimeException(e);
+        }
+    }
+    
+    @Before
+    public void persistReceipt() {
+        manager = factory.getEntityManager();
+        manager.getTransaction().begin();
+        manager.persist(schema.getReceiptSaleOne());
+        manager.getTransaction().commit();
+        manager.detach(schema.getReceiptSaleOne());
+    }
+}
