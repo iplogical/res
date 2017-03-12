32c401fed76b0994891c419e2944fcd4d4e449f2
Bálint-PC\Bálint <korcsmarb629@gmail.com>
2017-03-12 12:15:41 +0100

    Add missing functions to the RestaurantService.

--- app/src/main/java/com/inspirationlogical/receipt/service/RestaurantServices.java
+++ app/src/main/java/com/inspirationlogical/receipt/service/RestaurantServices.java
@@ -1,5 +1,30 @@
 package com.inspirationlogical.receipt.service;
 
+import java.util.List;
+
+import com.inspirationlogical.receipt.model.enums.TableType;
+import com.inspirationlogical.receipt.model.view.ReceiptRecordView;
+import com.inspirationlogical.receipt.view.RestaurantView;
+import com.inspirationlogical.receipt.view.TableView;
+
 public interface RestaurantServices {
+
+    List<TableView> getTables();
+
+    void setTableName(TableView table, String name);
+
+    void setTableCapacity(TableView table, int capacity);
+
+    void addTableNote(TableView table, String note);
+
+    void displayTable(TableView table);
+
+    void hideTable(TableView table);
+
+    void moveTable(TableView table, int coordinateX, int coordinateY);
+
+    TableView addTable(RestaurantView restaurant, TableType type, int tableNumber);
+
+    ReceiptRecordView getActiveReceipt(TableView table);
 
 }
--- app/src/main/java/com/inspirationlogical/receipt/service/RestaurantServicesImpl.java
+++ app/src/main/java/com/inspirationlogical/receipt/service/RestaurantServicesImpl.java
@@ -1,5 +1,51 @@
 package com.inspirationlogical.receipt.service;
 
+import java.util.List;
+
+import com.inspirationlogical.receipt.model.enums.TableType;
+import com.inspirationlogical.receipt.model.view.ReceiptRecordView;
+import com.inspirationlogical.receipt.view.RestaurantView;
+import com.inspirationlogical.receipt.view.TableView;
+
 public class RestaurantServicesImpl implements RestaurantServices {
+
+    @Override
+    public List<TableView> getTables() {
+        return null;
+    }
+
+    @Override
+    public void setTableName(TableView table, String name) {
+    }
+
+    @Override
+    public void setTableCapacity(TableView table, int capacity) {
+    }
+
+    @Override
+    public void addTableNote(TableView table, String note) {
+    }
+
+    @Override
+    public void displayTable(TableView table) {
+    }
+
+    @Override
+    public void hideTable(TableView table) {
+    }
+
+    @Override
+    public void moveTable(TableView table, int coordinateX, int coordinateY) {
+    }
+
+    @Override
+    public TableView addTable(RestaurantView restaurant, TableType type, int tableNumber) {
+        return null;
+    }
+
+    @Override
+    public ReceiptRecordView getActiveReceipt(TableView table) {
+        return null;
+    }
 
 }
