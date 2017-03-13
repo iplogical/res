7b497cb80a4d4181665f8ad079c1cd4667313176
Bálint-PC\Bálint <korcsmarb629@gmail.com>
2017-03-13 17:33:16 +0100

    Create new database ReceiptViewTest for view testing purposes.

--- app/src/test/java/com/inspirationlogical/receipt/model/EntityManagerFactoryRule.java
+++ app/src/test/java/com/inspirationlogical/receipt/model/EntityManagerFactoryRule.java
@@ -14,6 +14,13 @@ public class EntityManagerFactoryRule implements TestRule {
 
     private EntityManagerFactory emf;
     private EntityManager em;
+    private boolean viewTestSchema = false;
+
+    public EntityManagerFactoryRule(boolean viewTestSchema) {
+        this.viewTestSchema = viewTestSchema;
+    }
+
+    public EntityManagerFactoryRule() {}
 
     @Override
     public Statement apply(Statement base, Description description) {
@@ -22,7 +29,13 @@ public class EntityManagerFactoryRule implements TestRule {
             public void evaluate() throws Throwable {
                 Properties props = new Properties();
                 props.setProperty("persistenceXmlLocation", "resources/META-INF/persistence.xml");
-                emf = Persistence.createEntityManagerFactory("TestPersistance");
+                if(viewTestSchema) {
+                    props.setProperty("javax.persistence.jdbc.url", "jdbc:mysql://localhost:3306/ReceiptViewTest");
+                    props.setProperty("javax.persistence.schema-generation.database.action", "create");
+                    emf = Persistence.createEntityManagerFactory("TestPersistance", props);
+                } else {
+                    emf = Persistence.createEntityManagerFactory("TestPersistance");
+                }
                 em = emf.createEntityManager();
                 try {
                     base.evaluate();
---
+++ app/src/test/java/com/inspirationlogical/receipt/view/DatabaseCreator.java
@@ -0,0 +1,41 @@
+package com.inspirationlogical.receipt.view;
+
+import com.inspirationlogical.receipt.model.BuildTestSchemaRule;
+import com.inspirationlogical.receipt.model.EntityManagerFactoryRule;
+import com.inspirationlogical.receipt.model.entity.Restaurant;
+import com.inspirationlogical.receipt.service.RestaurantServices;
+import com.inspirationlogical.receipt.service.RestaurantServicesImpl;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+
+import javax.persistence.EntityManager;
+
+import static org.junit.Assert.assertNotNull;
+
+/**
+ * Created by Bálint on 2017.03.13..
+ */
+public class DatabaseCreator {
+    private EntityManager manager;
+    RestaurantServices service;
+
+    @Rule
+    public final EntityManagerFactoryRule factory = new EntityManagerFactoryRule(true);
+
+    @Rule
+    public final BuildTestSchemaRule schema = new BuildTestSchemaRule();
+
+    @Before
+    public void persistObjects() {
+        manager = factory.getEntityManager();
+        manager.getTransaction().begin();
+        manager.persist(schema.getRestaurant());
+        manager.getTransaction().commit();
+    }
+
+    @Test
+    public void buildTestDatabase() {
+        assertNotNull(manager.createNamedQuery(Restaurant.GET_ACTIVE_RESTAURANT).getResultList());
+    }
+}
