003d8d08273ad52f479f054b3d564426039aa204
Ferenc Janky <fecjanky@gmail.com>
2017-03-14 23:08:54 +0100

    Unit test speedup

--- app/src/test/java/com/inspirationlogical/receipt/model/EntityManagerFactoryRule.java
+++ app/src/test/java/com/inspirationlogical/receipt/model/EntityManagerFactoryRule.java
@@ -12,9 +12,28 @@ import org.junit.runners.model.Statement;
 
 public class EntityManagerFactoryRule implements TestRule {
 
-    private EntityManagerFactory emf;
+    static private EntityManagerFactory emf;
     private EntityManager em;
     private TestType testType = TestType.DROP_AND_CREATE;
+
+    static{
+        Properties props = new Properties();
+        TestType  testType = TestType.DROP_AND_CREATE;
+        if(testType == TestType.CREATE) {
+            props.setProperty("persistenceXmlLocation", "resources/META-INF/persistence.xml");
+            props.setProperty("javax.persistence.jdbc.url", "jdbc:mysql://localhost:3306/ReceiptViewTest");
+            props.setProperty("javax.persistence.schema-generation.database.action", "drop-and-create");
+            emf = Persistence.createEntityManagerFactory("TestPersistance", props);
+        } else if (testType == TestType.VALIDATE){
+            props.setProperty("persistenceXmlLocation", "resources/META-INF/persistence.xml");
+            props.setProperty("javax.persistence.jdbc.url", "jdbc:mysql://localhost:3306/ReceiptViewTest");
+            props.setProperty("javax.persistence.schema-generation.database.action", "validate");
+            emf = Persistence.createEntityManagerFactory("TestPersistance", props);
+        } else if(testType == TestType.DROP_AND_CREATE){
+            emf = Persistence.createEntityManagerFactory("TestPersistance");
+        }
+        emf = Persistence.createEntityManagerFactory("TestPersistance");
+    }
 
     public EntityManagerFactoryRule(TestType testType) {
         this.testType = testType;
@@ -27,26 +46,12 @@ public class EntityManagerFactoryRule implements TestRule {
         return new Statement() {
             @Override
             public void evaluate() throws Throwable {
-                Properties props = new Properties();
-                if(testType == TestType.CREATE) {
-                    props.setProperty("persistenceXmlLocation", "resources/META-INF/persistence.xml");
-                    props.setProperty("javax.persistence.jdbc.url", "jdbc:mysql://localhost:3306/ReceiptViewTest");
-                    props.setProperty("javax.persistence.schema-generation.database.action", "drop-and-create");
-                    emf = Persistence.createEntityManagerFactory("TestPersistance", props);
-                } else if (testType == TestType.VALIDATE){
-                    props.setProperty("persistenceXmlLocation", "resources/META-INF/persistence.xml");
-                    props.setProperty("javax.persistence.jdbc.url", "jdbc:mysql://localhost:3306/ReceiptViewTest");
-                    props.setProperty("javax.persistence.schema-generation.database.action", "validate");
-                    emf = Persistence.createEntityManagerFactory("TestPersistance", props);
-                } else if(testType == TestType.DROP_AND_CREATE){
-                        emf = Persistence.createEntityManagerFactory("TestPersistance");
-                }
+
                 em = emf.createEntityManager();
                 try {
                     base.evaluate();
                 } finally {
                     em.close();
-                    emf.close();
                 }
             }
         };
