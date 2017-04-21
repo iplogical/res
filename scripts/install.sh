934b08da72a4cca5f7a10ad84bf08b3aaec4d616
Ferenc Janky <fecjanky@gmail.com>
2017-04-21 19:00:44 +0200

    added insall script

---
+++ scripts/install.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+SCRIPT_DIR=$(dirname $0)
+
+INSTALL_DIR="/opt/iplogical"
+
+pushd $SCRIPT_DIR/../app
+./gradlew installDist
+popd
+
+echo $SCRIPT_DIR
+if [[ -e $INSTALL_DIR ]]
+then
+rm -rf /tmp/iplogical_todelete
+mv $INSTALL_DIR /tmp/iplogical_todelete
+fi
+
+mkdir -p $INSTALL_DIR
+cp -r $SCRIPT_DIR/../app/install/* $INSTALL_DIR/
+
+# cleanup bat files 
+rm -f $INSTALL_DIR/bin/*.bat
+# setup permission and owners of binaires
+chown manager:root $INSTALL_DIR/bin/manager
+chown manager:root $INSTALL_DIR/bin/sampleapp
+chown waiter:root $INSTALL_DIR/bin/waiter
+
+chmod 744 $INSTALL_DIR/bin/manager
+chmod 744 $INSTALL_DIR/bin/sampleapp
+chmod 744 $INSTALL_DIR/bin/waiter
+
+
