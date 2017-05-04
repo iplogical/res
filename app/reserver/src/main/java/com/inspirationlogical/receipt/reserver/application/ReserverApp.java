fb00e934c733d8bb2a77d63a378824bac908defd
Attila Gombos <attila.gombos.hu@gmail.com>
2017-05-04 21:19:21 +0200

    Update reserver app, fix dropwizard dependencies

--- app/reserver/src/main/java/com/inspirationlogical/receipt/reserver/application/ReserverApp.java
+++ app/reserver/src/main/java/com/inspirationlogical/receipt/reserver/application/ReserverApp.java
@@ -2,6 +2,12 @@ package com.inspirationlogical.receipt.reserver.application;
 
 import static com.inspirationlogical.receipt.reserver.registry.ReserverRegistry.getInstance;
 
+import java.util.EnumSet;
+import javax.servlet.DispatcherType;
+import javax.servlet.FilterRegistration;
+
+import org.eclipse.jetty.servlets.CrossOriginFilter;
+
 import com.inspirationlogical.receipt.reserver.configuration.ReserverConfiguration;
 import com.inspirationlogical.receipt.reserver.healthcheck.ReservationHealthCheck;
 import com.inspirationlogical.receipt.reserver.resource.ReservationResource;
@@ -14,7 +20,7 @@ import io.dropwizard.views.ViewBundle;
 public class ReserverApp extends Application<ReserverConfiguration> {
 
     private static final String SERVER_MODE = "server";
-    private static final String CONFIG_PATH = "reserver/src/main/resources/configuration/config.yml";
+    private static final String CONFIG_PATH = "/configuration/config.yml";
 
     private ReservationResource reservationResource;
 
@@ -26,7 +32,7 @@ public class ReserverApp extends Application<ReserverConfiguration> {
         if (args.length == 0) {
             args = new String[2];
             args[0] = SERVER_MODE;
-            args[1] = CONFIG_PATH;
+            args[1] = ReserverApp.class.getResource(CONFIG_PATH).getPath();
         }
         new ReserverApp(getInstance(ReservationResource.class)).run(args);
     }
@@ -47,6 +53,14 @@ public class ReserverApp extends Application<ReserverConfiguration> {
         final ReservationHealthCheck healthCheck = new ReservationHealthCheck();
         environment.healthChecks().register("reservation", healthCheck);
         environment.jersey().register(reservationResource);
+
+        FilterRegistration.Dynamic filter = environment.servlets().addFilter("CORS", CrossOriginFilter.class);
+        filter.addMappingForUrlPatterns(EnumSet.allOf(DispatcherType.class), true, "/*");
+        filter.setInitParameter("allowedOrigins", "http://localhost:9000/reservations, http://websocket.dnet.hu:9000/reservation");    // allowed origins comma separated
+        filter.setInitParameter("allowedHeaders", "Content-Type,Authorization,X-Requested-With,Content-Length,Accept,Origin");
+        filter.setInitParameter("allowedMethods", "GET,PUT,POST,DELETE,OPTIONS");
+        filter.setInitParameter("preflightMaxAge", "5184000"); // 2 months
+        filter.setInitParameter("allowCredentials", "true");
     }
 
 }
