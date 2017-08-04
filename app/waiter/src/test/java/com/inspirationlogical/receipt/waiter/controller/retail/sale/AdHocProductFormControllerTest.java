47309b876258a234c1ceec7483cb57b994b932db
TheDagi <korcsmarb629@gmail.com>
2017-08-04 21:59:59 +0200

    Move AdHocProductForm tests to separate class.

---
+++ app/waiter/src/test/java/com/inspirationlogical/receipt/waiter/controller/retail/sale/AdHocProductFormControllerTest.java
@@ -0,0 +1,39 @@
+package com.inspirationlogical.receipt.waiter.controller.retail.sale;
+
+import com.inspirationlogical.receipt.corelib.utility.Resources;
+import org.junit.Test;
+
+import static com.inspirationlogical.receipt.waiter.utility.ClickUtils.*;
+import static com.inspirationlogical.receipt.waiter.utility.ClickUtils.clickButtonThenWait;
+import static com.inspirationlogical.receipt.waiter.utility.ClickUtils.verifyThatNotVisible;
+import static com.inspirationlogical.receipt.waiter.utility.JavaFXIds.*;
+import static com.inspirationlogical.receipt.waiter.utility.JavaFXIds.ADHOC_PRODUCT_SALE_PRICE;
+import static com.inspirationlogical.receipt.waiter.utility.SaleUtils.assertSoldProduct;
+import static com.inspirationlogical.receipt.waiter.utility.SaleUtils.selectiveCancellation;
+import static com.inspirationlogical.receipt.waiter.utility.SaleUtils.sellAdHocProduct;
+
+public class AdHocProductFormControllerTest extends SaleViewTest {
+
+    @Test
+    public void testSellAdHocProduct() {
+        sellAdHocProduct("TestAdHocProductName", 2, 200, 400);
+        assertSoldProduct(1, "TestAdHocProductName", 2, 400, 800);
+        selectiveCancellation("TestAdHocProductName");
+        verifyThatNotVisible(Resources.WAITER.getString("AdHocProductForm.Title"));
+    }
+
+    @Test
+    public void testSellAdHocProductInvalidInput() {
+        clickButtonThenWait(SELL_ADHOC_PRODUCT, 100);
+        verifyThatVisible(Resources.WAITER.getString("AdHocProductForm.Title"));
+        setTextField(ADHOC_PRODUCT_NAME, "TestAdHocProductName");
+        setTextField(ADHOC_PRODUCT_QUANTITY, "Invalid");
+        setTextField(ADHOC_PRODUCT_PURCHASE_PRICE, Integer.toString(500));
+        setTextField(ADHOC_PRODUCT_SALE_PRICE, Integer.toString(500));
+        clickButtonThenWait("Form.Confirm", 100);
+        verifyThatVisible(Resources.WAITER.getString("AdHocProductForm.NumberFormatError"));
+        sleep(5100);
+        clickButtonThenWait("Form.Cancel", 100);
+        verifyThatNotVisible(Resources.WAITER.getString("AdHocProductForm.Title"));
+    }
+}
--- app/waiter/src/test/java/com/inspirationlogical/receipt/waiter/controller/retail/sale/SaleControllerTest.java
+++ app/waiter/src/test/java/com/inspirationlogical/receipt/waiter/controller/retail/sale/SaleControllerTest.java
@@ -17,76 +17,6 @@ import static org.junit.Assert.assertTrue;
 public class SaleControllerTest extends SaleViewTest {
 
     @Test
-    public void testClickOnGuestPlusThenGuestMinus() {
-        guestPlus();
-        assertEquals(TABLE_NUMBER + " (" + (Integer.valueOf(TABLE_GUESTS) + 1) + "/" + TABLE_CAPACITY + ")", ((Label)find(SALE_TABLE_NUMBER)).getText());
-        guestMinus();
-        assertEquals(TABLE_NUMBER + " (" + TABLE_GUESTS + "/" + TABLE_CAPACITY + ")", ((Label)find(SALE_TABLE_NUMBER)).getText());
-    }
-
-    @Test
-    public void testClickOnGuestMinusNegativeNotAllowed() {
-        guestMinus();
-        guestMinus();
-        guestMinus();
-        assertEquals(TABLE_NUMBER + " (" + TABLE_GUESTS + "/" + TABLE_CAPACITY + ")", ((Label)find(SALE_TABLE_NUMBER)).getText());
-    }
-
-    @Test
-    public void testTableSummaryTotalPrice() {
-        assertSoldTotalPrice(0);
-        selectCategory(BEERS);
-        sellProduct(SOPRONI);
-        assertSoldTotalPrice(440);
-        sellProduct(SOPRONI);
-        assertSoldTotalPrice(880);
-        sellProduct(KRUSO);
-        assertSoldTotalPrice(1360);
-        clickOnThenWait(KRUSO_LONG, 50);
-        assertSoldTotalPrice(1840);
-        selectiveCancellation(SOPRONI_LONG);
-        assertSoldTotalPrice(960);
-        selectiveCancellation(KRUSO_LONG);
-        assertSoldTotalPrice(0);
-        sellProduct(SOPRONI, 3);
-        assertSoldTotalPrice(1320);
-        singleCancellation(SOPRONI_LONG);
-        assertSoldTotalPrice(880);
-        singleCancellation(SOPRONI_LONG);
-        assertSoldTotalPrice(440);
-        singleCancellation(SOPRONI_LONG);
-        assertSoldTotalPrice(0);
-    }
-
-    @Test
-    public void testSortByClickTime() {
-        selectCategory(BEERS);
-        sellProduct(SOPRONI);
-        sellProduct(KRUSO);
-        sellProduct(EDELWEISS);
-        assertEquals(SOPRONI_LONG, getSoldProductName(1));
-        assertEquals(KRUSO_LONG, getSoldProductName(2));
-        assertEquals(EDELWEISS_LONG, getSoldProductName(3));
-
-        clickOnThenWait(SOPRONI_LONG, 50);
-        clickButtonThenWait(SORT_BY_CLICK_TIME, 50);
-        assertEquals(KRUSO_LONG, getSoldProductName(1));
-        assertEquals(EDELWEISS_LONG, getSoldProductName(2));
-        assertEquals(SOPRONI_LONG, getSoldProductName(3));
-        clickButtonThenWait(SORT_BY_CLICK_TIME, 50);
-
-        clickOnThenWait(KRUSO_LONG, 50);
-        clickButtonThenWait(SORT_BY_CLICK_TIME, 50);
-        assertEquals(EDELWEISS_LONG, getSoldProductName(1));
-        assertEquals(SOPRONI_LONG, getSoldProductName(2));
-        assertEquals(KRUSO_LONG, getSoldProductName(3));
-
-        selectiveCancellation(EDELWEISS_LONG);
-        selectiveCancellation(SOPRONI_LONG);
-        selectiveCancellation(KRUSO_LONG);
-    }
-
-    @Test
     public void testSellProduct() {
         selectCategory(BEERS);
         sellProduct(SOPRONI);
@@ -94,28 +24,6 @@ public class SaleControllerTest extends SaleViewTest {
         selectiveCancellation(SOPRONI_LONG);
     }
 
-    @Test
-    public void testSellAdHocProduct() {
-        sellAdHocProduct("TestAdHocProductName", 2, 200, 400);
-        assertSoldProduct(1, "TestAdHocProductName", 2, 400, 800);
-        selectiveCancellation("TestAdHocProductName");
-        verifyThatNotVisible(Resources.WAITER.getString("AdHocProductForm.Title"));
-    }
-
-    @Test
-    public void testSellAdHocProductInvalidInput() {
-        clickButtonThenWait(SELL_ADHOC_PRODUCT, 100);
-        verifyThatVisible(Resources.WAITER.getString("AdHocProductForm.Title"));
-        setTextField(ADHOC_PRODUCT_NAME, "TestAdHocProductName");
-        setTextField(ADHOC_PRODUCT_QUANTITY, "Invalid");
-        setTextField(ADHOC_PRODUCT_PURCHASE_PRICE, Integer.toString(500));
-        setTextField(ADHOC_PRODUCT_SALE_PRICE, Integer.toString(500));
-        clickButtonThenWait("Form.Confirm", 100);
-        verifyThatVisible(Resources.WAITER.getString("AdHocProductForm.NumberFormatError"));
-        sleep(5100);
-        clickButtonThenWait("Form.Cancel", 100);
-        verifyThatNotVisible(Resources.WAITER.getString("AdHocProductForm.Title"));
-    }
 
     @Test
     public void testSellGiftProduct() {
@@ -183,5 +91,75 @@ public class SaleControllerTest extends SaleViewTest {
         assertSoldSoproni(1, 1);
         singleCancellation(SOPRONI_LONG);
         assertNoSoldProduct();
+    }
+
+    @Test
+    public void testTableSummaryTotalPrice() {
+        assertSoldTotalPrice(0);
+        selectCategory(BEERS);
+        sellProduct(SOPRONI);
+        assertSoldTotalPrice(440);
+        sellProduct(SOPRONI);
+        assertSoldTotalPrice(880);
+        sellProduct(KRUSO);
+        assertSoldTotalPrice(1360);
+        clickOnThenWait(KRUSO_LONG, 50);
+        assertSoldTotalPrice(1840);
+        selectiveCancellation(SOPRONI_LONG);
+        assertSoldTotalPrice(960);
+        selectiveCancellation(KRUSO_LONG);
+        assertSoldTotalPrice(0);
+        sellProduct(SOPRONI, 3);
+        assertSoldTotalPrice(1320);
+        singleCancellation(SOPRONI_LONG);
+        assertSoldTotalPrice(880);
+        singleCancellation(SOPRONI_LONG);
+        assertSoldTotalPrice(440);
+        singleCancellation(SOPRONI_LONG);
+        assertSoldTotalPrice(0);
+    }
+
+    @Test
+    public void testClickOnGuestPlusThenGuestMinus() {
+        guestPlus();
+        assertEquals(TABLE_NUMBER + " (" + (Integer.valueOf(TABLE_GUESTS) + 1) + "/" + TABLE_CAPACITY + ")", ((Label)find(SALE_TABLE_NUMBER)).getText());
+        guestMinus();
+        assertEquals(TABLE_NUMBER + " (" + TABLE_GUESTS + "/" + TABLE_CAPACITY + ")", ((Label)find(SALE_TABLE_NUMBER)).getText());
+    }
+
+    @Test
+    public void testClickOnGuestMinusNegativeNotAllowed() {
+        guestMinus();
+        guestMinus();
+        guestMinus();
+        assertEquals(TABLE_NUMBER + " (" + TABLE_GUESTS + "/" + TABLE_CAPACITY + ")", ((Label)find(SALE_TABLE_NUMBER)).getText());
+    }
+
+    @Test
+    public void testSortByClickTime() {
+        selectCategory(BEERS);
+        sellProduct(SOPRONI);
+        sellProduct(KRUSO);
+        sellProduct(EDELWEISS);
+        assertEquals(SOPRONI_LONG, getSoldProductName(1));
+        assertEquals(KRUSO_LONG, getSoldProductName(2));
+        assertEquals(EDELWEISS_LONG, getSoldProductName(3));
+
+        clickOnThenWait(SOPRONI_LONG, 50);
+        clickButtonThenWait(SORT_BY_CLICK_TIME, 50);
+        assertEquals(KRUSO_LONG, getSoldProductName(1));
+        assertEquals(EDELWEISS_LONG, getSoldProductName(2));
+        assertEquals(SOPRONI_LONG, getSoldProductName(3));
+        clickButtonThenWait(SORT_BY_CLICK_TIME, 50);
+
+        clickOnThenWait(KRUSO_LONG, 50);
+        clickButtonThenWait(SORT_BY_CLICK_TIME, 50);
+        assertEquals(EDELWEISS_LONG, getSoldProductName(1));
+        assertEquals(SOPRONI_LONG, getSoldProductName(2));
+        assertEquals(KRUSO_LONG, getSoldProductName(3));
+
+        selectiveCancellation(EDELWEISS_LONG);
+        selectiveCancellation(SOPRONI_LONG);
+        selectiveCancellation(KRUSO_LONG);
     }
 }
